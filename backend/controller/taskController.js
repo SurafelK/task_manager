2b8a79071c4dd1921f48a02d10a3845119d2226b
Surafel Kassahun <babishufer@gmail.com>
2023-12-22 16:13:51 +0300

    Changes on Routes and Controller

--- backend/controller/taskController.js
+++ backend/controller/taskController.js
@@ -1,5 +1,6 @@
 const Task = require("../models/taskModel");
 
+/**Create Task */
 const createTask = async (req,res)=>
 {   
     try{
@@ -11,6 +12,7 @@ const createTask = async (req,res)=>
        }
 };
 
+/**Get Tasks */
 const getTasks = async(req,res) =>
 {
     try{
@@ -21,7 +23,73 @@ const getTasks = async(req,res) =>
     }
 }
 
+/**Get a Single Task */
+const getTask = async (req,res) =>
+{
+   
+   
+   try
+   {
+    const {id} = req.params;
+    const task = await Task.findById(id);
+
+    if(!task)
+    {
+        return res.status(404).json("No Task with id" + id)
+    }
+
+    res.status(200).json(task)
+   }catch(error)
+   {
+    res.send({message:error.message})
+   }
+}
+/**Delete Task */
+const deleteTask = async (req,res)=>
+{
+    try{
+        const {id} =req.params;
+        const task = await Task.findByIdAndDelete(id)
+
+        if(!task)
+        {
+           return res.status(404).json("Task couldn't be found")
+        }
+
+        res.status(200).json("Task Deleted")
+    }catch(error)
+    {
+        res.send(500).json({msg:error.message})
+    }
+}
+
+
+const updateTask = async(req,res) =>
+{
+    try {
+        const {id} = req.params;
+        const task = await Task.findByIdAndUpdate(
+            {_id:id}, req.body, {
+                new: true,
+                runValidators:true}
+        );
+
+        if(!task)
+        {
+            return res.status(404).json("No task with id: "+ id)
+        }
+
+        res.status(200).json(task)
+    } catch (error) {
+        res.status(500).json({msg:error.message})
+        
+    }
+}
+
 module.exports = {
     createTask,
-    getTasks
+    getTasks,
+    getTask,
+    deleteTask,
+    updateTask
 }
--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -2,10 +2,13 @@ const express = require('express');
 
 const routes = express.Router();
 const Task  = require('../models/taskModel');
-const { createTask, getTasks } = require('../controller/taskController');
+const { createTask, getTasks, getTask, deleteTask, updateTask } = require('../controller/taskController');
 
     routes.post("/api/tasks", createTask)
     routes.get('/api/tasks', getTasks )
+    routes.get('/api/task/:id',getTask)
+    routes.delete("/api/task/:id",deleteTask)
+    routes.patch('/api/tasks/:id',updateTask);
 
 
     module.exports = routes;
